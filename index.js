8f7dd25ed8a29e939c708b1807f9f91923d3fe13
not-wish <visheshagarwaltsk@gmail.com>
2025-02-03 19:45:00 +0530

    Intial Commit

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const Product = require("./models/product.models.js");
-const productRoute = require("./routes/product.routes.js");
+const express = require("express"); // import Express
+const mongoose = require("mongoose"); // import Mongoose
+const productRoute = require("./routes/product.routes.js"); // import the route(s)
 
 const app = express();
 
 //middleware
-app.use(express.json());
+app.use(express.json()); 
 app.use(express.urlencoded({ extended: false }));
 
 //routes
 app.use("/api", productRoute);
 
+// MongoDB connection
 mongoose
   .connect(
     "yourMongoDB connection link here"
   )
- 
-
   .then(() => {
     console.log("Connected to the database!");
     app.listen(3000, () => {
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const router = express.Router();
-const Product = require("../models/product.models.js");
+const router = express.Router(); 
+
 const {
   getProducts,
   getProduct,
